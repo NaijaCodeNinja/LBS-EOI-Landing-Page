2c0492a0fca30c0a3b645dc6e48d6808f0c7885d
NaijaCodeNinja <davechuks06@gmail.com>
2026-08-13 13:31:49 +0100

    Got everything set up properly; changed around the file structure a bit (Added Backend and Frontend folders separately); Reinstalled PHP and did a bit of tinkering with the `.ini` file; renamed something in the Programme of Interest schema.

--- Backend/database/migrations/2026_08_12_094937_create_programme_interests_table.php
+++ Backend/database/migrations/2026_08_12_094937_create_programme_interests_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('programme_interests', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('full_name');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->string('programme'); // e.g., "Full-Time MBA", "Executive Education"
+            $table->text('message')->nullable(); // Optional field for notes
+            $table->timestamps(); // Creates created_at and updated_at columns automatically
         });
     }
 
